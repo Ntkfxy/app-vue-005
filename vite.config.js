aae9663bac3987db7672f71de7386f2f1c76d412
Ntkfxy <natthaan57101@gmail.com>
2026-03-02 09:45:02 +0700

    update build deploy

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vue-app/',
+  base: '/vue-app-005/',
   plugins: [vue()],
   resolve: {
     alias: {
